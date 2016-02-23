4024074456d3365c5b64036418e625a8994a9c1a
Allen Gao <allen001@live.ca>
2016-02-23 18:25:04 -0500

    wip

--- src/CenterPanel.java
+++ src/CenterPanel.java
@@ -1,17 +1,35 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JColorChooser;
 import javax.swing.JPanel;
 
 public class CenterPanel extends JPanel {
+	
+	public int x;
+	public int y;
+	
 	public CenterPanel() {
 		super();
 		this.setOpaque(true);
 		this.setBackground(Color.WHITE);
+		//this.add(new JColorChooser());
+		
+		MouseAdapter mouseAdapter = new MouseAdapter() {
+			public void mouseMoved(MouseEvent e) {
+				x = e.getX();
+				y = e.getY();
+				repaint();
+			}
+		};
+		
+		this.addMouseMotionListener(mouseAdapter);
 	}
 	
 	public void paintComponent(Graphics g) {
-		//g.setColor(Color.WHITE);
-		//g.fillRect(0, 0, 600, 600);
+		g.setColor(Color.BLACK);
+		g.fillRect(x, y, 5, 5);
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,16 +1,19 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 	public GamePanel() {
+		System.out.println("gfdgdf");
 		this.setLayout(new BorderLayout());
 		this.add(new SouthPanel(), BorderLayout.SOUTH);
 		this.add(new CenterPanel(), BorderLayout.CENTER);
 		this.add(new WestPanel(),  BorderLayout.WEST);
 		this.add(new NorthPanel(), BorderLayout.NORTH);
 		
-		this.setBackground(Color.RED);
+		this.setBackground(Color.WHITE);
 	}
 }
