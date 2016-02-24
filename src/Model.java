2c4ba354d1ed672d7e060145b62f4e010a1d7811
Allen Gao <allen.gao@uwaterloo.ca>
2016-02-24 14:09:27 -0500

    colour pallet wip

--- src/Model.java
+++ src/Model.java
@@ -1,7 +1,42 @@
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Observable;
 
 public class Model extends Observable {
+	
+	private ArrayList<Color> colorArray;
+	private Color currentColor;
+	
 	public Model() {
+		colorArray = new ArrayList<Color>(Arrays.asList(
+				Color.BLACK, 
+				Color.BLUE,
+				Color.CYAN,
+				Color.DARK_GRAY,
+				Color.GRAY,
+				Color.GREEN,
+				Color.YELLOW,
+				Color.LIGHT_GRAY,
+				Color.MAGENTA,
+				Color.ORANGE,
+				Color.PINK,
+				Color.RED,
+				Color.WHITE)
+		);
 		
 	}
+	
+	public ArrayList<Color> getColorArray() {
+		return this.colorArray;
+	}
+
+	public Color getCurrentColor() {
+		return currentColor;
+	}
+
+	public void setCurrentColor(Color currentColor) {
+		this.currentColor = currentColor;
+	}
+	
 }
--- src/WestPanel.java
+++ src/WestPanel.java
@@ -1,5 +1,9 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -10,14 +14,28 @@ import javax.swing.JPanel;
 public class WestPanel extends JPanel implements Observer {
 	
 	private Model model;
+	private ArrayList<Color> colorArray;
+	private int preferredWidth = 60;
 	
 	public WestPanel(Model model) {
 		this.model = model;
 		model.addObserver(this);
-		
-		this.add(new JButton("test"));
 		this.setBackground(Color.WHITE);
 		this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		this.setPreferredSize(new Dimension(this.preferredWidth, this.getHeight()));
+		
+		this.colorArray = model.getColorArray();
+		for (int i = 0; i < this.colorArray.size(); i++) {
+			JButton colorButton = new JButton();
+			colorButton.setBackground(this.colorArray.get(i));
+			colorButton.setPreferredSize(new Dimension(20, 20));
+			colorButton.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+					model.setCurrentColor(colorButton.getBackground());
+				}
+			});
+			this.add(colorButton);
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
